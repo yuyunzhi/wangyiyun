b7108e5905f44769a493b1e4e58dcf2b9a865848
e <lambert1129@foxmail.com>
2018-06-03 23:25:57 +0800

    更改路径

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(function(){
                 <h3>${i.name}</h3>
                 <div class='sq'></div>
                 <p>${i.descript}</p>
-                <a href="../html/song.html?id=${i.id}">
+                <a href="./song.html?id=${i.id}">
                     <svg class="icon" aria-hidden="true">
                         <use xlink:href="#icon-play1"></use>
                     </svg>

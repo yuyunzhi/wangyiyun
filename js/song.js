368e9e4583d159746ee33a8bb5f68cc7d8c6f0a5
e <lambert1129@foxmail.com>
2018-06-03 20:18:55 +0800

    播放音乐和歌词

--- js/song.js
+++ js/song.js
@@ -1,7 +1,6 @@
 $(function(){
 
     let id = location.search.match(/\bid=([^&]*)/)[1]
-//播放歌
     $.get('../json/song.json').then(function(response){
         let songs = response
         let song = songs.filter((s)=>{
@@ -9,54 +8,58 @@ $(function(){
         })
         let url = song[0].url
         let name = song[0].descript
-        $('.song-description>h2').text(name)
-        let audio = document.createElement('audio') 
-        audio.src=url
-        audio.oncanplay=function(){
-            audio.play()
-            $('.disc').addClass('active')
-            $('.icon-pause').addClass('playing')
-        }
-        $('.icon-pause').on('click',function(){
-            $('.icon-pause').removeClass('playing')
-            $('.icon-play').addClass('playing')
-            $('.disc').removeClass('active')
-            audio.pause();
-        })
-        $('.icon-play').on('click',function(){
-            $('.icon-play').removeClass('playing')
-            $('.icon-pause').addClass('playing')
-            $('.disc').addClass('active')
-            audio.play();
-        })
+        let lyric = song[0].lyric
+        initPlayer(url)
+        initText(name,lyric)
+    })
+    
+})
+
+
+
         
-    })
-
-    //歌词
-    $.get('../json/lyric.json').then(function(object){
-        let lyric = object.lyric
-        let array = lyric.split('\n')
-        let regex = /^\[(.+)\](.*)$/
-        array = array.map(function(string,index){
-            let matches = string.match(regex)
-            if(matches){
-                return {
-                    time:matches[1],
-                    words:matches[2]
-                }
-            }
-        })
-
-        let $line = $('.lyric>.line')
-        array.map(function(object){
-            let $p = $(`<p>${object.words}</p>`)
-            $p.attr('data-time',object.time)
-            $line.append($p)
-        })
-
-    })
-
-
+function initPlayer(url){
+    $('.song-description>h2').text(name)
+    let audio = document.createElement('audio') 
+    audio.src=url
+    audio.oncanplay=function(){
+        audio.play()
+        $('.disc').addClass('active')
+        $('.icon-pause').addClass('playing')
     }
+    $('.icon-pause').on('click',function(){
+        $('.icon-pause').removeClass('playing')
+        $('.icon-play').addClass('playing')
+        $('.disc').removeClass('active')
+        audio.pause();
+    })
+    $('.icon-play').on('click',function(){
+        $('.icon-play').removeClass('playing')
+        $('.icon-pause').addClass('playing')
+        $('.disc').addClass('active')
+        audio.play();
+    })
+    setInterval(()=>{
+        console.log(audio.currentTime)
+    },1000)
+}
 
-)
+function initText(name,lyric){
+    let array = lyric.split('\n')
+    let regex = /^\[(.+)\](.*)$/
+    array = array.map(function(string,index){
+        let matches = string.match(regex)
+        if(matches){
+            return {
+                time:matches[1],
+                words:matches[2]
+            }
+        }
+    })
+    let $line = $('.lyric>.line')
+    array.map(function(object){
+        let $p = $(`<p>${object.words}</p>`)
+        $p.attr('data-time',object.time)
+        $line.append($p)
+    })
+}
